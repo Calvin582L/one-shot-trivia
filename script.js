914a194bbc90520fe8dac7dc33f39dc27f98e326
Calvin Liew <70549389+Calvin582L@users.noreply.github.com>
2021-07-25 15:38:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const api_url = "https://opentdb.com/api.php?amount=1";
+if(localStorage.getItem("highscore") == null){
+    localStorage.setItem("highscore", 0);
+  }
 document.getElementById("highscore").innerHTML = "Current High Score: " + "<strong>" + localStorage.getItem("highscore")+"</strong>";  
 
 let points = 0;
@@ -63,9 +66,6 @@ function endgame(gameover){
   if (points > localStorage.getItem("highscore")){
     localStorage.setItem("highscore", points);
     document.getElementById("highscore").innerHTML = "*NEW* High Score: " + "<strong>" + localStorage.getItem("highscore")+"</strong>"; 
-  }
-  else if(localStorage.getItem("highscore") == null){
-    localStorage.setItem("highscore", 0);
   }
   else{
     document.getElementById("highscore").innerHTML = "High Score: " + "<strong>" + localStorage.getItem("highscore")+"</strong>";  
